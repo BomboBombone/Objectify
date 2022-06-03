a6b860e009e271ded6be36e752ec9e9f4c470194
paciaronimarco10@gmail.com <paciaronimarco10@gmail.com>
2022-06-03 23:02:38 +0200

    Initial commit

---
+++ objectify.py
@@ -0,0 +1,20 @@
+import sys
+import os
+
+content = '//embeds.h\n' \
+          '#pragma once\n' \
+          '#include <Windows.h>\n' \
+          '\n'
+
+i = 1
+for arg in sys.argv[1:]:
+    file = open(arg, 'rb')
+    file_bytes = list(file.read(os.path.getsize(arg)))
+    file.close()
+
+    content += 'BYTE embedded_image_' + str(i) + '[] = ' + str(file_bytes).replace('[', '{').replace(']', '}') + ';\n\n'
+    i += 1
+
+output = open('embeds.h', 'wb')
+output.write(bytes(content, 'utf-8'))
+output.close()
