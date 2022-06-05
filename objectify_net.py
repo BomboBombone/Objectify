648fd8b6e157e9a7830c72236a52044874717c44
paciaronimarco10@gmail.com <paciaronimarco10@gmail.com>
2022-06-05 19:37:33 +0200

    Added support for C# conversions too

---
+++ objectify_net.py
@@ -0,0 +1,23 @@
+import sys
+import os
+
+content = '//embeds.cs\n' \
+          'using System\n' \
+          '\n' \
+          'namespace Program {\n' \
+          '\tpublic class Embeds {\n'
+
+i = 1
+for arg in sys.argv[1:]:
+    file = open(arg, 'rb')
+    file_bytes = list(file.read(os.path.getsize(arg)))
+    file.close()
+
+    content += '\t\tpublic static byte[] embedded_image_' + str(i) + ' = ' + str(file_bytes).replace('[', '{').replace(']', '}') + ';\n\n'
+    i += 1
+
+content += '\t}\n}\n'
+
+output = open('embeds.cs', 'wb')
+output.write(bytes(content, 'utf-8'))
+output.close()
